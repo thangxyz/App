9e2cb9db47041fc58ef0dc39358939d0064a29c8
your name <truongdinhthangxyz@gmail.com>
2023-05-16 03:35:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {
+  Routes, Route,Link
+} from "react-router-dom";
+import Home from'./component/Home';
+import About from'./component/About';
+import Product from'./component/Product';
+import Contact from'./component/Contact';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">Trang Chủ</Link>
+        <Link to="/about">Sản Phẩm</Link>
+        <Link to="/product">Giới Thiệu</Link>
+        <Link to="/contact">Liên Hệ</Link>
+       </nav>
+       <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/about" element={<About/>}/>
+       <Route path="/product" element={<Product/>}/>
+       <Route path="/contact" element={<Contact/>}/>
+
+       
+       </Routes>
+      
     </div>
   );
 }
